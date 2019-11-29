ff1e9d6f434893df17ec113238dd925cbf462f01
100288 <100288@MACTECH.LOC>
2019-11-29 10:27:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/epantionmodel/ExpansionPanelSampleActivity.java
@@ -0,0 +1,14 @@
+package com.example.epantionmodel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExpansionPanelSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.expansion_panel_sample_main);
+    }
+}
